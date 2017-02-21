b28c0c2a7edcb3d2f49e9243af1b6e2a53f57b15
Jerrie Pelser <jerrie@jerriepelser.com>
2017-02-21 22:05:04 +0900

    A basic Lock implementation added

---
+++ src/GeoToastWebClient/src/components/Login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div id="lock"></div>
+  </div>
+</template>
+
+<script>
+  import Auth0Lock from 'auth0-lock'
+
+  export default {
+    name: 'login',
+    mounted () {
+      const lock = new Auth0Lock('sAlZCm26CewF4re702Av7fMotT3KG027', 'jerrie.auth0.com', {
+        container: 'lock'
+      })
+
+      lock.on('authenticated', (authResult) => {
+        localStorage.setItem('id_token', authResult.idToken)
+        this.$router.push('/')
+      })
+
+      lock.show()
+    }
+  }
+
+</script>
--- src/GeoToastWebClient/src/router/index.js
+++ src/GeoToastWebClient/src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from 'components/Hello'
+import Login from 'components/Login'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
   ]
 })
